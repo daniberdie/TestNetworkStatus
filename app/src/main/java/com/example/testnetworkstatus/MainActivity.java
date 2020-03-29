2859205ad8b38287f4cf2bc1b88df9b8b80517c9
Nil Agut Marin <nilagut@gmail.com>
2020-03-29 12:55:57 +0200

    Exercice A

--- app/src/main/java/com/example/testnetworkstatus/MainActivity.java
+++ app/src/main/java/com/example/testnetworkstatus/MainActivity.java
@@ -6,42 +6,44 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textView1,textView2;
+public class MainActivity extends AppCompatActivity
+{
+    TextView textView1, textView2;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if(networkInfo != null){
+
+        if (networkInfo != null) {
             textView1.setText(networkInfo.toString());
-            if(networkInfo.isConnected()) {
-                if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+            if (networkInfo.isConnected())
+            {
+                if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
                 {
                     textView2.setText(R.string.wifi);
                 }
-                if(networkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+                if (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
                 {
                     textView2.setText(R.string.mobile);
                 }
-            }else{
+            }
+            else
+            {
                 textView2.setText(R.string.disconnected);
             }
-
         }
     }
 }
